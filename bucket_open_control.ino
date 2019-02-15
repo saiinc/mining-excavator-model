50474d27441ebca703ea21e831d4aca974650922
Saionji Kyouichi <saiinc@nm.ru>
2019-02-15 13:03:02 +0300

    Update bucket_open_control.ino

--- bucket_open_control.ino
+++ bucket_open_control.ino
@@ -3,12 +3,15 @@
 Servo servo1; //объявляем переменную servo типа Servo
 boolean butt_flag = 0;
 boolean butt;
-//boolean servo_flag;
+unsigned long last_time;
+int rot_spd = 20;
+int ugol1;
 
 void setup() //процедура setup
-{
+{  
   servo1.attach(10); //привязываем привод к порту 10
-  pinMode(3, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);  
+  servo1.write(5);    
 }
 
 void loop() //процедура loop
@@ -16,23 +19,18 @@ void loop() //процедура loop
   butt = !digitalRead(3); //считать текущее положение кнопки
   if (butt == true && butt_flag == false) //кнопка нажата
     {
-      butt_flag = true;
-      servo1.write(1); //ставим вал под 1
-      delay(2000); //ждем 2 секунды
-      servo1.write(179); //ставим вал под 179
-      delay(2000); //ждем 2 секунды
-      butt_flag = false;
-      //servo_flag = !servo_flag;
+      butt_flag = true;      
+      for(ugol1 = 5; ugol1 <= 155; ugol1++)
+      {
+        servo1.write(ugol1); 
+        delay(rot_spd);
+      }      
+      delay(rot_spd*4);
+      for(ugol1 = 155; ugol1 >= 5; ugol1--)
+      {
+        servo1.write(ugol1); 
+        delay(rot_spd);
+      }            
+      butt_flag = false;      
     }
-/*
-   if (butt == false && butt_flag == true) //кнопка отпущена
-    {
-      butt_flag = false;
-    }
-  
-  servo1.write(1); //ставим вал под 1
-  delay(2000); //ждем 2 секунды
-  servo1.write(179); //ставим вал под 179
-  delay(2000); //ждем 2 секунды
-  */
 }
