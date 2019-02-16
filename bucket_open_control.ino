717494ab5f3a92716c810e230e2da1a6a97a023a
Saionji <saiinc@narod.ru>
2019-02-16 12:50:35 +0300

    angle mod

--- bucket_open_control.ino
+++ bucket_open_control.ino
@@ -4,13 +4,13 @@ Servo servo1; //объявляем переменную servo типа Servo
 boolean butt_flag = 0;
 boolean butt;
 unsigned long last_time;
-int rot_spd = 20;
+int rot_spd = 20; //скорость поворота сервы
 int ugol1;
 
 void setup() //процедура setup
 {  
   servo1.attach(10); //привязываем привод к порту 10
-  pinMode(3, INPUT_PULLUP);  
+  pinMode(3, INPUT_PULLUP);  //привязываем кнопку к порту 3
   servo1.write(5);    
 }
 
@@ -20,13 +20,13 @@ void loop() //процедура loop
   if (butt == true && butt_flag == false) //кнопка нажата
     {
       butt_flag = true;      
-      for(ugol1 = 5; ugol1 <= 155; ugol1++)
+      for(ugol1 = 5; ugol1 <= 165; ugol1++) //медленно открываем днище ковша
       {
         servo1.write(ugol1); 
         delay(rot_spd);
       }      
       delay(rot_spd*4);
-      for(ugol1 = 155; ugol1 >= 5; ugol1--)
+      for(ugol1 = 165; ugol1 >= 5; ugol1--) //возвращаемся в исходное положение
       {
         servo1.write(ugol1); 
         delay(rot_spd);
