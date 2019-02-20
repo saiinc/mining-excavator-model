bdead891ee76f89b13cb0ab911aae0dcc00d14ba
Saionji <saiinc@narod.ru>
2019-02-20 18:35:23 +0300

    delay

--- bucket_open_control.ino
+++ bucket_open_control.ino
@@ -1,14 +1,21 @@
 #include <Servo.h> //используем библиотеку для работы с сервоприводом
+#include <Bounce2.h>
+#define PIN_BUTTON 3
 Servo servo1; //объявляем переменную servo типа Servo
-boolean butt_flag = 0;
+boolean butt_flag = false;
 boolean butt;
+boolean butt_old = false;
 int rot_spd = 20; //скорость поворота сервы
 int ugol1;
+Bounce debouncer = Bounce();
 
 void setup() //процедура setup
 {    
   Serial.begin(9600);
-  pinMode(3, INPUT_PULLUP);  //привязываем кнопку к порту 3
+  //pinMode(3, INPUT);  //привязываем кнопку к порту 3
+  pinMode(PIN_BUTTON, INPUT_PULLUP);  //привязываем кнопку к порту 3
+  debouncer.attach(PIN_BUTTON);
+  debouncer.interval(25);
   pinMode(4, OUTPUT);
   servo1.attach(10); //привязываем привод к порту 10
   servo1.write(5);
@@ -17,7 +24,11 @@ void setup() //процедура setup
 
 void loop() //процедура loop
 {
-  butt = !digitalRead(3); //считать текущее положение кнопки
+  debouncer.update();
+  Serial.print(!digitalRead(3));
+  butt = debouncer.fell(); //считать текущее положение кнопки
+  //if (butt != butt_old) { delay(50); }
+  //butt = !digitalRead(3);
   if (butt == true && butt_flag == false) //кнопка нажата
     {
       butt_flag = true;     
